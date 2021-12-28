5f10cbf521526bbcc10d05e67ddcc58842822bdf
lzy <lzy>
2021-12-28 20:44:19 +0800

    init

--- libgit.py
+++ libgit.py
@@ -56,13 +56,20 @@ def MSB_len(lenth):
 			result = bnum[(i+1) * 7:] + result
 	return result
 
-def write_head(pack_file):
+def write_head(pack_file, lenth):
 	sha1 = hashlib.sha1()
 	pack_file.write(b"PACK")
 	sha1.update(b"PACK")
 	pack_file.write(struct.pack('>I', 2))
 	sha1.update(struct.pack('>I', 2))
+	pack_file.write(struct.pack('>I',lenth))#list的大小要有限制，也就是文件数量不大于4G
+	sha1.update(struct.pack('>I', lenth))
 	return sha1
+def head_data(lenth):
+	result = b"PACK"
+	result += struct.pack('>I', 2)
+	result += struct.pack('>I',lenth)
+	return result
 
 def write_tail(old_name, sha1_hash):
 	pack_name = sha1_hash
@@ -72,33 +79,50 @@ def write_tail(old_name, sha1_hash):
 	pack_file.close()
 	os.system("git index-pack " + pack_file.name)
 
-def new_pack(object_list, repo_name):
+def obj_data(obj):
+	f = obj.read_raw()
+	head = 0x00 ^ (obj.type << 4)      #type
+	MSB = MSB_len(len(f))
+	head =  head ^ int(MSB[0:4], base = 2)
+	MSB = MSB[4:]
+	n = int(len(MSB) / 8)
+	if n >= 1:
+		head = (head ^ 0x80)
+	data = struct.pack('B', head)
+	for i in range(0,n):
+		data += struct.pack('B', int(MSB[i*8:(i+1)*8], base = 2))
+	###以上是type和长度######
+	data += zlib.compress(f)
+	return data
+
+def new_pack_By_data(da, lenth):
+	print(lenth, end = " ")
+	print("start writing:")
+	data = head_data(lenth) + da    
+	sha1 = hashlib.sha1()
+	sha1.update(data)
+	pack_name = sha1.hexdigest()
+	data += Binary(pack_name)
+	pack_file = open('.git/objects/pack/pack-'  + pack_name + ".pack", 'wb')
+	pack_file.write(data)
+	pack_file.close()
+	os.system("git index-pack " + pack_file.name)
+
+
+
+def new_pack_By_list(object_list, repo_name):
 	print(len(object_list), end = " ")
 	print("start writing:")
 	repo = Repository('.git')
 	pack_file = open('.git/objects/pack/pack-.pack', 'wb')
-	sha1 = write_head(pack_file)
 	l = len(object_list)
-	pack_file.write(struct.pack('>I',l ))#list的大小要有限制，也就是文件数量不大于4G
-	sha1.update(struct.pack('>I', l))
+	sha1 = write_head(pack_file, l)
 	num = 0
 	for file in object_list:#对每个文件写入
 		num += 1
 		print("\rpacking：%.2f%%" %(float(num/l*100)),end='')
 		obj = repo.get(file)
-		f = obj.read_raw()
-		head = 0x00 ^ (obj.type << 4)      #type
-		MSB = MSB_len(len(f))
-		head =  head ^ int(MSB[0:4], base = 2)
-		MSB = MSB[4:]
-		n = int(len(MSB) / 8)
-		if n >= 1:
-			head = (head ^ 0x80)
-		data = struct.pack('B', head)
-		for i in range(0,n):
-			data += struct.pack('B', int(MSB[i*8:(i+1)*8], base = 2))
-		###以上是写入type和长度######
-		data += zlib.compress(f)
+		data = obj_data(obj)
 		pack_file.write(data)
 		sha1.update(data)
 	check_sum = sha1.hexdigest()
@@ -112,57 +136,52 @@ def walk_commit_tree(this_commit):
 	repo = Repository('.git')
 	if this_commit not in li:
 		li.add(this_commit)
-	new_size = len(repo.get(this_commit).read_raw())	
+	data = obj_data(repo.get(this_commit))	
 	queue = [repo.get(this_commit).tree]
 	while len(queue) > 0:
 		this_tree = queue.pop(0)
 		if str(this_tree.id) in li:
 			continue
 		li.add(str(this_tree.id))
-		new_size += len(this_tree.read_raw())
+		data += obj_data(this_tree)
 		for t in this_tree:
 			if str(t.id) in li:
 				continue
 			if t.type == types["OBJ_BLOB"]:
 				li.add(str(t.id))
-				new_size += t.size
+				data += obj_data(t)
 			if t.type == types["OBJ_TREE"]:
 				queue.append(t)
-	return new_size
+	return data
 
 def pack_by_commit(commit_list, repo_name, single_size = 0):
 	global li
-	pack_size = 0
+	lenth = 0
+	data = b''
 	for commit in commit_list:
-		if pack_size >= single_size:
-			new_pack(li, repo_name)
+		if len(data) >= single_size:
+			new_pack_By_data(data, len(li))
 			li = set()
-			pack_size = walk_commit_tree(commit)
+			data = walk_commit_tree(commit)
 			continue
-		pack_size += walk_commit_tree(commit)
-	new_pack(li, repo_name)
+		data += walk_commit_tree(commit)
+	new_pack_By_data(data, len(li))
 
 
 
 
 		
-pack = "neo4j"
+pack = "C:\\Users\\dell\\Desktop\\neo4j"
 os.chdir(pack)
 
 
 # for t in repo.get("d6b0236bd288faafd2e879fcb6677c34544322ab").tree:
 # 	print(t.name)
 
-# all_refs = list(repo.references)
-# for r in all_refs:
-# 	if type(repo.references[r].target) != str:
-# 		for commit in repo.walk(repo.references[r].target):
-# 			if commit.id not in commit_list:
-# 				commit_list.add(commit.id)
-# 				print(commit.id)
+
 commit_list = get_all_commit(pack)
-pack_by_commit(commit_list, pack, 4<<30)
+pack_by_commit(commit_list, pack, 1<<30)
 
 
-
-# new_pack(li, pack)
+# li = get_all_objs(pack)
+# new_pack_By_list(li,pack)
