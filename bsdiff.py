35c41b906770deda20a1d600ed8009d53c7d36e5
lzylhh <louzy@pku.edu.cn>
2023-02-23 21:41:30 +0800

    repack compelete

--- bsdiff.py
+++ bsdiff.py
@@ -60,92 +60,80 @@ num = 0
 def get_diff(src_bytes, dst_bytes):
 	global num
 	len_dst, tcontrol, bdiff, bextra = ddiff(src_bytes, dst_bytes)
-	# len1 = len(src_bytes)
-	# len2 = len(dst_bytes)
-	# delta_data = bytearray()
-	# #先写入头size
-	# delta_data += libgit.size_format(len1)
-	# delta_data += libgit.size_format(len2)
-	# #重新编码数据
-	# tlist = []
-	# oldpos = 0
-	# diffpos = 0
-	# expos = 0
-	
-	# for i in range(len_dst):
-	# 	tup = tcontrol[i]
-	# 	x = tup[0]
-	# 	y = tup[1]
-	# 	z = tup[2]
-	# 	if x == 0:
-
-
-
-	for k in bdiff:
-		if k !=0:
-			print(k)
-	num += len(bextra)
-	# d = open("diff", 'rb')
-	# len_dst, tcontrol, bdiff, bextra = read_patch(d)
-	l = len(tcontrol)
-	delta_data = bytearray()
-	#先写入头size
 	len1 = len(src_bytes)
 	len2 = len(dst_bytes)
+	delta_data = bytearray()
+	#先写入头size
 	delta_data += libgit.size_format(len1)
 	delta_data += libgit.size_format(len2)
+	#重新编码数据
+	tlist = []
 	oldpos = 0
 	diffpos = 0
 	expos = 0
-	oldata = bytearray(src_bytes)
-
-	i = 0
-	final = 0
-	while i < l:
+	newpos = 0
+	nextra = bytearray()
+	for i in range(len_dst):
 		tup = tcontrol[i]
 		x = tup[0]
 		y = tup[1]
 		z = tup[2]
-		#处理copy部分大小为0的情况
 		if x == 0:
-			ex_data = bytearray(bextra[expos: expos + y])
-			ex_len  = len(ex_data)
-			add_pos = 0
-			while add_pos < ex_len:
-				if add_pos + 127 < ex_len:
-					delta_data += (struct.pack('B', 0x7f) + ex_data[add_pos:add_pos+127])
-					add_pos += 127
-				else:
-					delta_data += (struct.pack('B', ex_len - add_pos) + ex_data[add_pos:ex_len])
-					break
-			expos += y
+			if y > 0:
+				nextra += bextra[expos:expos+y]
+				tlist.append([0,newpos,newpos+y])
+				newpos += y
+				expos += y
 			oldpos += z
+			continue
+		#处理copy部分
+		diffpoint = -1
+		for j in range(x):
+			#处理不一样的异样点
+			if bdiff[diffpos + j] != 0:
+				if diffpoint+1 < j:
+					tlist.append([1,oldpos+diffpoint+1,j-diffpoint-1])#偏移和size,在两个异样点之间有的话
+				c = bdiff[diffpos+j] + src_bytes[oldpos+j]
+				if c > 255:
+			 		c -= 256
+				nextra += struct.pack('B', c)
+				tlist.append([0,newpos,newpos+1])
+				newpos += 1
+				diffpoint = j
+				continue
+			if j == x-1:#遍历到末尾之后
+				if diffpoint < j:
+					tlist.append([1,oldpos+diffpoint+1,j-diffpoint])#偏移和size,在两个异样点之间有的话
+		oldpos += x+z
+		diffpos += x
+		#处理新增部分
+		if y > 0:
+			nextra += bextra[expos:expos+y]
+			tlist.append([0,newpos,newpos+y])
+			newpos += y
+			expos += y
+	#进行数据写入
+	tlen = len(tlist)
+	i = 0
+	while i < tlen:
+		t = tlist[i]
+		if t[0] == 1 and len(del_instruction.copy_format(t[1], t[2])) < t[2] + 1:
+			delta_data += del_instruction.copy_format(t[1], t[2])
 			i += 1
 			continue
-		#如果copy部分大小不为0，则进行编码指令选择
-		offset = oldpos
-		size = x
-		copy_fomat = del_instruction.copy_format(offset, size)
-		copy_size = len(copy_fomat)
-		if copy_size >= size + 1:
-			lianxu = 0
+		else:
 			ex_data = bytearray()
-			cs = copy_size
-			while i < l:
-				lianxu += tcontrol[i][0]
-				ex_data += oldata[oldpos: oldpos+tcontrol[i][0]]
-				lianxu += tcontrol[i][1]
-				if tcontrol[i][1] > 0:
-					ex_data += bextra[expos: expos + tcontrol[i][1]]
-					expos += tcontrol[i][1]
-				oldpos += tcontrol[i][0] + tcontrol[i][2]
+			while i < tlen:
+				if tlist[i][0] == 0:
+					ex_data += nextra[tlist[i][1]:tlist[i][2]]
+				else:
+					ex_data += src_bytes[tlist[i][1]:tlist[i][1]+tlist[i][2]]
 				i += 1
-				if i >= l:
+				if i >= tlen:
 					break
-				cs = len(del_instruction.copy_format(oldpos, tcontrol[i][0]))
-				if cs < tcontrol[i][0]:
+				if tlist[i][0] == 1 and len(del_instruction.copy_format(tlist[i][1], tlist[i][2])) <= tlist[i][2]:
 					break
-		#把所有的add数据写入
+			#把所有的add数据写入
 			ex_len  = len(ex_data)
 			add_pos = 0
 			while add_pos < ex_len:
@@ -155,26 +143,9 @@ def get_diff(src_bytes, dst_bytes):
 				else:
 					delta_data += (struct.pack('B', ex_len - add_pos) + ex_data[add_pos:ex_len])
 					break
-		else:#如果copy部分更优，就编码为copy指令
-			# print(offset)
-			# print(size)
-			
-			delta_data += copy_fomat
-			if y > 0:
-				ex_data = bytearray(bextra[expos: expos + y])
-				ex_len  = len(ex_data)
-				add_pos = 0
-				while add_pos < ex_len:
-					if add_pos + 127 < ex_len:
-						delta_data += (struct.pack('B', 0x7f) + ex_data[add_pos:add_pos+127])
-						add_pos += 127
-					else:
-						delta_data += (struct.pack('B', ex_len - add_pos) + ex_data[add_pos:ex_len])
-						break
-			oldpos += z + x
-			expos += y
-			i += 1
 	return delta_data
+
+
 	
 def test(delta_data,data1,data2):
 	# os.chdir(path)
@@ -313,8 +284,8 @@ def test1(path):
 	repo = Repository(".git")
 	# file1 = open("1.txt",'rb').read()
 	# file2 = open("2.txt",'rb').read()
-	data2 = repo.get("ade99119da14f4ebd6f1e049d5e1f14d71610043").read_raw()
-	data1 = repo.get("3ba60cc2eefece2ccbdfc555a975604e0e2d23f5").read_raw()
+	data2 = repo.get("1ebb9f7cf95c1f1fcfb2b738bc3ff820a55904c6").read_raw()
+	data1 = repo.get("e5d4440f31721c43a5be213e858ff6dbe165e018").read_raw()
 	delta_data = get_diff(data1, data2)
 	#验证正确性
 	point = -1
@@ -359,7 +330,10 @@ def test1(path):
 					b = delta_data[point]
 					size += b<<(8*i)				
 				chizi <<= 1
-			now_data += data1[offset:(offset+size)]
+			if size == 0:
+				now_data += data1[offset:(offset+0x10000)]
+			else:
+				now_data += data1[offset:(offset+size)]
 			num3 += size
 		#新数据指令
 		else:
@@ -371,20 +345,26 @@ def test1(path):
 			# print(data.decode('utf-8'))
 			point += data_size
 	# print(num3)
-	# if len(data2) != len(now_data):
-	# 	print(1)
+	if len(data2) != len(now_data):
+		print(1)
 	# l = len(data2)
+	# print(l)
 	# for i in range(l):
 	# 	if now_data[i] != data2[i]:
 	# 		print(now_data[i])
-	# print(data2[1001:].decode("utf-8"))
+	# print(data2.decode("utf-8"))
 	# nd = now_data.decode("utf-8")
+	# file3 = open("res.js",'wb')
+	# file1 = open("1.js",'wb')
+	# file2 = open("2.js",'wb')
+	# file3.write(now_data)
+	# file2.write(data2)
+	# file1.write(data1)
 
 def main():
-	repack("C:\\Users\\dell\\Desktop\\xgboost")
-# test1("C:\\Users\\dell\\Desktop\\xgboost")
+	repack("C:\\Users\\dell\\Desktop\\kubernetes")
+# test1("C:\\Users\\dell\\Desktop\\three.js")
 main()
-print(num)
 # def repack1(path):
 # 	os.chdir(path)
 # 	repo = Repository(".git")
