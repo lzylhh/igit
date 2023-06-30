2c08a852b43d626b5636fccfd126de98a2a31756
lzylhh <louzy@pku.edu.cn>
2023-06-30 11:03:51 +0800

    final

--- new_pack.py
+++ new_pack.py
@@ -2,7 +2,20 @@ import simulate
 import bsdiff
 import time
 import timeit
+import sys
+import bsdiff
 def main(path):
     sortlist = simulate.get_pack_base(path)
     bsdiff.Repack(path, sortlist)
-print(timeit.timeit(stmt=lambda:main("C:\\Users\\dell\\Desktop\\server"), number=1))
+if __name__ == '__main__':
+    # print(sys.argv)
+    if len(sys.argv) == 3:
+        if sys.argv[1] == '-f':
+            bsdiff.repack(sys.argv[2])
+        else:
+            print("Wrong Input")
+    elif len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print("Wrong Input")
+# print(timeit.timeit(stmt=lambda:main("C:\\Users\\dell\\Desktop\\server"), number=1))
